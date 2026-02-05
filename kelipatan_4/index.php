32adeca685f7e8ad31f6c4b42dd2f8ebb2894102
genta <gentapossenti@gmail.com>
2026-02-05 20:09:31 +0700

    soal kelipatan 4

---
+++ kelipatan_4/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kelipatan 4</title>
+</head>
+<body>
+
+<?php
+$i = 4;      
+$no = 1;     
+
+while ($i <= 40) {
+    
+    for ($j = $no; $j <= $no; $j++) {
+        echo "total angka pada kelipatan ke-$j | $i <br>";
+    }
+
+    $i += 4;
+    $no++;
+}
+?>
+
+</body>
+</html>
